2ba9823a0e3fb41bd9ce453a7baa2140cca9a987
Bisquitman <lynx8309@gmail.com>
2022-03-11 12:22:51 +0300

    Day3. Ceate Modal

--- js/index.js
+++ js/index.js
@@ -1,96 +1,36 @@
-// Сортировка и период выборки
 const optionBtnOrder = document.querySelector('.option__btn_order');
 const optionListOrder = document.querySelector('.option__list_order');
 const optionBtnPeriod = document.querySelector('.option__btn_period');
 const optionListPeriod = document.querySelector('.option__list_period');
 
-// Кнопка "По дате"
-optionBtnOrder.addEventListener('click', () => {
-  optionListOrder.classList.toggle('option__list_active');
-  optionListPeriod.classList.remove('option__list_active');
-});
+const found = document.querySelector('.found');
 
-// Кнопка "За всё время"
-optionBtnPeriod.addEventListener('click', () => {
-  optionListPeriod.classList.toggle('option__list_active');
-  optionListOrder.classList.remove('option__list_active');
-});
-
-// Список "По дате..."
-optionListOrder.addEventListener('click', (event) => {
-  const target = event.target;
-  if (target.classList.contains('option__item')) {
-    optionBtnOrder.textContent = target.textContent;
-    optionListOrder.classList.remove('option__list_active');
-    for (const elem of optionListOrder.querySelectorAll('.option__item')) {
-      if (elem === target) {
-        elem.classList.add('option__item_active');
-      } else {
-        elem.classList.remove('option__item_active');
-      }
-    }
-  }
-});
-
-// Список "За всё время..."
-optionListPeriod.addEventListener('click', (event) => {
-  const target = event.target;
-  if (target.classList.contains('option__item')) {
-    optionBtnPeriod.textContent = target.textContent;
-    optionListPeriod.classList.remove('option__list_active');
-    for (const elem of optionListPeriod.querySelectorAll('.option__item')) {
-      if (elem === target) {
-        elem.classList.add('option__item_active');
-      } else {
-        elem.classList.remove('option__item_active');
-      }
-    }
-  }
-});
-
-// Выбор города
 const topCityBtn = document.querySelector('.top__city');
 const city = document.querySelector('.city');
 const cityClose = document.querySelector('.city__close');
 const cityRegionList = document.querySelector('.city__region-list');
 
-// Показываем большой список городов
-topCityBtn.addEventListener('click', () => {
-  city.classList.toggle('city_active');
-});
-
-// Выбираем город
-cityRegionList.addEventListener('click', (event) => {
-  const target = event.target;
-
-  if (target.classList.contains('city__link')) {
-    topCityBtn.textContent = target.textContent;
-    city.classList.remove('city_active');
-  }
-});
-// Закрываем окно с городами по крестику
-cityClose.addEventListener('click', () => {
-  city.classList.remove('city_active');
-});
-
-// Модальное окно
 const overlayVacancy = document.querySelector('.overlay_vacancy');
 const resultList = document.querySelector('.result__list');
 
-resultList.addEventListener('click', (e) => {
-  const target = e.target;
+const formSearch = document.querySelector('.bottom__search');
 
-  if (target.dataset.vacancy) {
-    e.preventDefault();
-    overlayVacancy.classList.add('overlay_active');
-  }
-});
+//* Функции
 
-overlayVacancy.addEventListener('click', (e) => {
-  if (e.target === overlayVacancy || e.target.classList.contains('modal__close')) {
-    overlayVacancy.classList.remove('overlay_active');
+const getData = ({ search, id } = {}) => {
+  if (search) {
+    return fetch(`http://localhost:3000/api/vacancy?search=${search}`).then(response => response.json());
   }
-});
+  return fetch(`http://localhost:3000/api/vacancy/${id ? id : ''}`).then(response => response.json());
+};
+
+
+// Склонение числительных
+const declOfNum = (n, titles) => {
+  return n + ' ' + titles[n % 10 === 1 && n % 100 !== 11 ?
+    0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+};
+
 
 // Вывод карточек
 const createCard = (vacancy) => {
@@ -148,32 +88,207 @@ const renderCards = (data) => {
 };
 
 
-const getData = ({ search } = {}) => {
-  if (search) {
-    return fetch(`http://localhost:3000/api/vacancy?search=${search}`).then(response => response.json());
-  }
-  return fetch('http://localhost:3000/api/vacancy').then(response => response.json());
-};
-
-const formSearch = document.querySelector('.bottom__search');
-formSearch.addEventListener('submit', async (e) => {
-  e.preventDefault();
-  const textSearch = formSearch.search.value; // .search. - атрибут name инпута
-  if (textSearch.length > 2) {
-    formSearch.search.style.borderColor = '';
-    const data = await getData({ search: textSearch });
-    console.log('data: ', data.length);
-    renderCards(data);
-    formSearch.reset();
-  } else {
-    formSearch.search.style.borderColor = 'red';
-    setTimeout(() => { formSearch.search.style.borderColor = '' }, 2000);
-  }
-});
-
 const init = async () => {
   const data = await getData();
-  console.log('data: ', data);
   renderCards(data);
+
+
+  //* Обработчики событий
+
+  // Кнопка "По дате"
+  optionBtnOrder.addEventListener('click', () => {
+    optionListOrder.classList.toggle('option__list_active');
+    optionListPeriod.classList.remove('option__list_active');
+  });
+
+  // Кнопка "За всё время"
+  optionBtnPeriod.addEventListener('click', () => {
+    optionListPeriod.classList.toggle('option__list_active');
+    optionListOrder.classList.remove('option__list_active');
+  });
+
+  // Список "По дате..."
+  optionListOrder.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.classList.contains('option__item')) {
+      optionBtnOrder.textContent = target.textContent;
+      optionListOrder.classList.remove('option__list_active');
+      for (const elem of optionListOrder.querySelectorAll('.option__item')) {
+        if (elem === target) {
+          elem.classList.add('option__item_active');
+        } else {
+          elem.classList.remove('option__item_active');
+        }
+      }
+    }
+  });
+
+  // Список "За всё время..."
+  optionListPeriod.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.classList.contains('option__item')) {
+      optionBtnPeriod.textContent = target.textContent;
+      optionListPeriod.classList.remove('option__list_active');
+      for (const elem of optionListPeriod.querySelectorAll('.option__item')) {
+        if (elem === target) {
+          elem.classList.add('option__item_active');
+        } else {
+          elem.classList.remove('option__item_active');
+        }
+      }
+    }
+  });
+
+  // Показываем большой список городов
+  topCityBtn.addEventListener('click', () => {
+    city.classList.toggle('city_active');
+  });
+
+  // Выбираем город
+  cityRegionList.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target.classList.contains('city__link')) {
+      topCityBtn.textContent = target.textContent;
+      city.classList.remove('city_active');
+    }
+  });
+
+  // Закрываем окно с городами по крестику
+  cityClose.addEventListener('click', () => {
+    city.classList.remove('city_active');
+  });
+
+  // Создание модального окна
+  const createModal = (data) => {
+    console.log('data: ', data);
+    const {
+      address,
+      compensation,
+      description,
+      employer,
+      employment,
+      experience,
+      skills,
+      title
+    } = data;
+
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    const closeButtonElem = document.createElement('button');
+    closeButtonElem.classList.add('modal__close');
+    closeButtonElem.textContent = '✕';
+
+    const titleElem = document.createElement('h2');
+    titleElem.classList.add('modal__title');
+    titleElem.textContent = title;
+
+    const compensationElem = document.createElement('p');
+    compensationElem.classList.add('modal__compensation');
+    compensationElem.textContent = compensation;
+
+    const employerElem = document.createElement('p');
+    employerElem.classList.add('modal__employer');
+    employerElem.textContent = employer;
+
+    const addressElem = document.createElement('p');
+    addressElem.classList.add('modal__address');
+    addressElem.textContent = address;
+
+    const experienceElem = document.createElement('p');
+    experienceElem.classList.add('modal__experience');
+    experienceElem.textContent = experience;
+
+    const employmentElem = document.createElement('p');
+    employmentElem.classList.add('modal__employment');
+    employmentElem.textContent = employment;
+
+    const descriptionElem = document.createElement('p');
+    descriptionElem.classList.add('modal__description');
+    descriptionElem.textContent = description;
+
+    const skillsElem = document.createElement('div');
+    skillsElem.classList.add('modal__skills', 'skills');
+
+    const skillsTitleElem = document.createElement('h3');
+    skillsTitleElem.classList.add('skills__title');
+    skillsTitleElem.textContent = 'Подробнее:';
+
+    const skillsListElem = document.createElement('ul');
+    skillsListElem.classList.add('skills__list');
+    for (const skill of skills) {
+      const skillsItemElem = document.createElement('li');
+      skillsItemElem.classList.add('skills__item');
+      skillsItemElem.textContent = skill;
+      skillsListElem.append(skillsItemElem);
+    }
+
+    const submitButtonElem = document.createElement('button');
+    submitButtonElem.classList.add('modal__response');
+    submitButtonElem.textContent = 'Отправить резюме';
+
+    skillsElem.append(skillsTitleElem, skillsListElem);
+
+    modal.append(
+      closeButtonElem,
+      titleElem,
+      compensationElem,
+      employerElem,
+      addressElem,
+      experienceElem,
+      employmentElem,
+      descriptionElem,
+      skillsElem,
+      submitButtonElem
+    );
+
+    return modal;
+  };
+
+  // Модальное окно
+  let modal = null;
+
+  // Открытие модального окна
+  resultList.addEventListener('click', async (e) => {
+    const target = e.target;
+
+    if (target.dataset.vacancy) {
+      e.preventDefault();
+      overlayVacancy.classList.add('overlay_active');
+      const data = await getData({ id: target.dataset.vacancy });
+      modal = createModal(data);
+      overlayVacancy.append(modal);
+    }
+  });
+
+  // Закрытие модального окна
+  overlayVacancy.addEventListener('click', (e) => {
+    if (e.target === overlayVacancy || e.target.classList.contains('modal__close')) {
+      overlayVacancy.classList.remove('overlay_active');
+      modal.remove();
+    }
+  });
+
+  // Поиск
+  formSearch.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const textSearch = formSearch.search.value; // .search. - атрибут name инпута
+    if (textSearch.length > 2) {
+      formSearch.search.style.borderColor = '';
+      const data = await getData({ search: textSearch });
+      renderCards(data);
+      found.innerHTML = `${declOfNum(data.length, ['вакансия', 'вакансии', 'вакансий'])} &laquo;${textSearch}&raquo;`;
+      formSearch.reset();
+    } else {
+      formSearch.search.style.borderColor = 'red';
+      setTimeout(() => { formSearch.search.style.borderColor = '' }, 2000);
+    }
+  });
+
+  //* --- end of Обработчики событий ---
 };
+
+//* --- end of Функции ---
+
 init();
